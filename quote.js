831995f8002999bd424aa3945a834410a90bdcb0
mariapaulav <mariapaulaviviz@gmail.com>
2022-08-09 15:52:06 -0500

    randomQuote Generator finished

---
+++ quote.js
@@ -0,0 +1,31 @@
+const quoteText = document.querySelector('.quote'),
+quoteAuthor = document.querySelector('.name'),
+quoteBtn = document.querySelector('button')
+soundBtn = document.querySelector('.sound')
+copyBtn = document.querySelector('.copy')
+
+function randomQuote(){
+   quoteBtn.innerText = '. . .'
+   fetch('http://api.quotable.io/random')
+   .then(response => response.json())
+   .then(result => {
+    console.log(result)
+    quoteText.innerText = result.content;
+    quoteAuthor.innerText = result.author;
+   quoteBtn.innerText = 'New Quote'
+   })
+}
+
+soundBtn.addEventListener('click', ()=> {
+    let speak = new SpeechSynthesisUtterance(quoteText.innerText)
+    //let speak = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${quoteAuthor.innerText}`)
+    speak.lang = 'en-US'
+    speechSynthesis.speak(speak)
+})
+
+copyBtn.addEventListener('click', ()=>{
+    navigator.clipboard.writeText(quoteText.innerText)
+});
+
+
+quoteBtn.addEventListener('click', randomQuote);
